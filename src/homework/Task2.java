5e8d92fadda9cbf73577ad8ff5f6711b24d831ab
Roman F <roman70rus@gmail.com>
2022-09-11 00:33:21 +0700

    Make corrections to the task 2

--- src/homework/Task2.java
+++ src/homework/Task2.java
@@ -2,11 +2,11 @@ package homework;
 
 public class Task2 {
 
-        /*
-        Task2
+    /*
+    Task2
 
-            [3, 4, 2, 7], 10 -> [3, 7] - вывести пару менно в скобках, которые дают сумму - 10
-         */
+        [3, 4, 2, 7], 10 -> [3, 7] - вывести пару менно в скобках, которые дают сумму - 10
+     */
 
     public static void main(String[] args) {
         int[] array = {3, 4, 2, 7};
@@ -14,15 +14,21 @@ public class Task2 {
     }
 
     public static void printPair(int[] array, int sum) {
-        if (array != null) {
-            for (int k : array) {
-                for (int i : array) {
-                    if (k + i == sum) {
-                        System.out.printf("[%d, %d]", k, i);
-                        return;
-                    }
+        if (array != null && array.length > 1) {
+            for (int i = 0, j = 1; i < array.length; j++) {
+                if (array[i] + array[j] == sum) {
+                    System.out.printf("[%d, %d]", array[i], array[j]);
+                    break;
+                }
+
+                if (j == array.length - 1) {
+                    j = i + 1;
+                    i++;
                 }
             }
+
+        } else {
+            System.out.println("Array is null or its size less than 2");
         }
     }
 }
